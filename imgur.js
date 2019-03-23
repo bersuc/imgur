84b475699c2af428b384b2310ed39034cef4e2b8
Diogo Bersuc <dbersuc@gmail.com>
2019-03-22 23:33:49 -0300

    Adding await to get right answer. More comments

--- imgur.js
+++ imgur.js
@@ -1,26 +1,28 @@
 const imgur = require('imgur')
-//const clientId = require('./keys.json').clientId
 const readlines = require('readline-sync')
 
-const conteudo = readlines.question('Ola, diga um animal em ingles: ');
-console.log(conteudo);
-console.log(typeof(conteudo));
+const conteudo = readlines.question('Hi! Say an animal name: ');
+console.log('You searched for: ' + conteudo);
 
-//imgur.setClientId(clientId)
 buscadorDeImagem(conteudo)
-.then(results => {
+.then(results => {  
   return results;
 })
 .catch(err => console.error(err));
 
 
 async function buscadorDeImagem(conteudo){
-  const optionalParams = {sort: 'top', dateRange: 'month', page: 1}
-  let response1 = await imgur.search(conteudo, optionalParams);
-  let linkResposta = response1['data'][0]['link'];
-  let respostaDados = response1['data'];
-  let tamanhoObjeto = Object.keys(respostaDados).length;
+  /*
+  sort - Accepts 'time', 'viral', or 'top' as a value. Defaults to top.
+  dateRange - Accepts 'day', 'week', 'month', 'year', or 'all' as a value. Defaults to all.
+  page - Accepts an integer (e.g. 1, 2, 3, 4) as a value. Defaults to 1.
+  */
+  const optionalParams = {sort: 'time', dateRange: 'all', page: 1}
+  let response1 =     await imgur.search(conteudo, optionalParams);
+  let linkResposta = await response1['data'][0]['link'];
+  let respostaDados = await response1['data'];
 
+  
   respostaDados.forEach(elemento => {
     console.log(elemento['link']);
   });  
